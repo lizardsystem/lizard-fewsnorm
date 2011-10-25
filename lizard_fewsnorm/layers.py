24019e11bbbaa80e7e73e68b2cf48b803f603421
jack <jack.ha@nelen-schuurmans.nl>
2011-10-25 15:45:33 +0200

    Busy with adapter

--- lizard_fewsnorm/layers.py
+++ lizard_fewsnorm/layers.py
@@ -1,14 +1,49 @@
+import mapnik
+
+from django.conf import settings
+
 from lizard_map.workspace import WorkspaceItemAdapter
+from lizard_fewsnorm.models import FewsNormSource
 
 
 class AdapterFewsNorm(WorkspaceItemAdapter):
     def __init__(self, *args, **kwargs):
         super(AdapterFewsNorm, self).__init__(*args, **kwargs)
-        # Something with layer_arguments specific for FewsNorm
+        self.parameter_id = self.layer_arguments['parameter_id']
+        self.module_id = self.layer_arguments['module_id']
+        self.fews_norm_source_slug = self.layer_arguments[
+            'fews_norm_source_slug']
 
     def layer(self, layer_ids=None, request=None):
-        """Generate layers and styles"""
-        pass
+        """Generate layers and styles
+
+        The layer is filtered using fews_norm_source_slug,
+        parameter_id and module_id"""
+        layers = []
+        styles = {}
+        #fews_norm_source = FewsNormSource(slug=self.fews_norm_source_slug)
+        query = (
+            """
+          (select geometry from lizard_fewsnorm_geolocationcache as loc,
+             lizard_fewsnorm_fewsnormsource as source where
+               loc.fews_norm_source_id = source.id and
+               source.slug = '%s'
+           ) data""" % self.fews_norm_source_slug)
+
+        default_database = settings.DATABASES['default']
+        datasource = mapnik.PostGIS(
+            host=default_database['HOST'],
+            port=default_database['PORT'],
+            user=default_database['USER'],
+            password=default_database['PASSWORD'],
+            dbname=default_database['NAME'],
+            table=query.encode('ascii')
+            )
+
+        layer = mapnik.Layer("FewsNorm", coordinates.WGS84)
+        layer.datasource = datasource
+
+        return layers, styles
 
     def search(self, x, y, radius=None):
         """Search by coordinates. Return list of dicts for matching

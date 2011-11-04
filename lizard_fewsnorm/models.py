ad4fa7f1e23dee04438cce5bd37d2c52675cc9c3
Alexandr Seleznev <a.seleznev@hotmail.nl>
2011-11-04 13:27:23 +0100

    Added get_timeserie and get_latest_value functions into TimeSeriesCache model.

--- lizard_fewsnorm/models.py
+++ lizard_fewsnorm/models.py
@@ -347,6 +347,25 @@ class TimeSeriesCache(models.Model):
             self.modulecache,
             self.timestepcache)
 
+    def get_timeserie(self, dt_start, dt_end):
+        source = self.geolocationcache.fews_norm_source()
+        timeserieskey = source.o(self.TimeseriesKey).filter(
+            locationkey__id=self.geolocationcache.ident,
+            parameterkey__id=self.parametercache.ident,
+            moduleinstancekey__id=self.modulecache.ident)
+        if len(timeserieskey) == 0:
+            return []
+        else:
+            serieskey = timeserieskey[0]
+            return serieskey.timeseriesvaluesandflags_set.objects.filter(
+                datetime__gte=dt_start,
+                datetime__lte=dt_end)
+
+    def get_latest_value(self, dt_start, dt_end):
+        timeserie = self.get_timeserie(dt_start, dt_end).order_by('-timestep')
+        if len(timeserie) > 0:
+            return timeserie[0]
+
 
 class FewsNormSource(models.Model):
     """

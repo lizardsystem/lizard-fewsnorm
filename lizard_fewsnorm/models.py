d3debd25b0fe834fa84d2301307c757425875226
jack <jack.ha@nelen-schuurmans.nl>
2011-11-10 12:20:37 +0100

    Removed unused and untested functions

--- lizard_fewsnorm/models.py
+++ lizard_fewsnorm/models.py
@@ -348,6 +348,7 @@ class TimeSeriesCache(models.Model):
             self.modulecache,
             self.timestepcache)
 
+<<<<<<< Updated upstream
     def get_timeserie(self, dt_start, dt_end):
         source = self.geolocationcache.fews_norm_source()
         timeserieskey = source.o(self.TimeseriesKey).filter(
@@ -366,6 +367,26 @@ class TimeSeriesCache(models.Model):
         timeserie = self.get_timeserie(dt_start, dt_end).order_by('-timestep')
         if len(timeserie) > 0:
             return timeserie[0]
+
+    # def get_timeserie(self, dt_start, dt_end):
+    #     source = self.geolocationcache.fews_norm_source()
+    #     timeserieskey = source.o(self.TimeseriesKey).filter(
+    #         locationkey__id=self.geolocationcache.ident,
+    #         parameterkey__id=self.parametercache.ident,
+    #         moduleinstancekey__id=self.modulecache.ident)
+    #     if len(timeserieskey) == 0:
+    #         return []
+    #     else:
+    #         serieskey = timeserieskey[0]
+    #         return serieskey.timeseriesvaluesandflags_set.objects.filter(
+    #             datetime__gte=dt_start,
+    #             datetime__lte=dt_end)
+
+    # def get_latest_value(self, dt_start, dt_end):
+    #     timeserie = self.get_timeserie(dt_start, dt_end).order_by('-timestep')
+    #     if len(timeserie) > 0:
+    #         return timeserie[0]
+
 
 
 class FewsNormSource(models.Model):
